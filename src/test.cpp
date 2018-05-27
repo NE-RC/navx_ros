d71598fcd63075c8d1f683d3f37fa0425f4324c8
Nicholas Liu <nicholas.j.liu@gmail.com>
2018-05-27 01:06:21 -0700

    cleaned up code

--- src/test.cpp
+++ src/test.cpp
@@ -1,21 +1,19 @@
-#include <iostream>
-#include "AHRS.h"
 #include "ros/ros.h"
+#include "std_msgs/Float32.h"
+#include <AHRS.h>
 
-int main(int argc, char **argv)
-{
-
-	ros::init(argc, argv, "navX");
-
+int main(int argc, char** argv){
+	ros::init(argc, argv, "test");
 	ros::NodeHandle nh;
-	
+	ros::Publisher pub = nh.advertise<std_msgs::Float32>("test", 1000);
+	ros::Rate period(10);
 
-	AHRS com = AHRS("/dev/ttyACM0");
-	while (true){
-		float vx = com.GetVelocityX();
-		float vy = com.GetVelocityY();
-		float vz = com.GetVelocityZ();
-		std::cout << vx << " " << vy << " " << vz << std::endl;
+	while(ros::ok()){
+		std_msgs::Float32 msg;		
+		msg.data = 3256;
+		pub.publish(msg);
+		ros::spinOnce();
+		period.sleep();
 	}
 	return 0;
 }
